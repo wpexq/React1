2db914a8bfe2224beebcfbbc9754bfce98bd9893
wpexq <pkq194@gmail.com>
2026-03-25 10:14:44 +0900

    test commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # React1
+test
---
+++ src/Profile.jsx
@@ -0,0 +1,9 @@
+import reactLogo from './assets/react.svg'
+
+export default function Profile() {
+  return(
+    <>
+      <img src={reactLogo} alt='리액트 로고'/>  
+    </>
+  )
+}
